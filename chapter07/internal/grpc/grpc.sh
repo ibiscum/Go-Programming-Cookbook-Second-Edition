3cb46cf2a67730acc6225495bab2c61124789780
ibiscum <ulf.luecking@gmail.com>
2024-02-16 20:06:33 +0100

    Protobuf.

--- chapter07/internal/grpc/grpc.sh
+++ chapter07/internal/grpc/grpc.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-protoc --go_out=plugins=grpc:. greeter/greeter.proto  
+# protoc --go_out=plugins=grpc:. greeter/greeter.proto  
 
-protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    helloworld/helloworld.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative greeter/greeter.proto
