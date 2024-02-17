f9a164b673db77d3d9179cc2fd2e7ea58c27a4a8
ibiscum <ulf.luecking@gmail.com>
2024-02-17 01:26:40 +0100

    Test.

--- chapter14/cmd/fastweb/items_test.go
+++ chapter14/cmd/fastweb/items_test.go
@@ -23,13 +23,18 @@ func TestAddItem(t *testing.T) {
 }
 
 func TestReadItems(t *testing.T) {
+	type args struct {
+		item string
+	}
 	tests := []struct {
 		name string
+		args args
 		want []string
 	}{
-		{"base-case", []string{"", "test"}},
+		{"base-case", args{"test"}, []string{"test"}},
 	}
 	for _, tt := range tests {
+		AddItem(tt.args.item)
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ReadItems(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ReadItems() = %v, want %v", got, tt.want)
