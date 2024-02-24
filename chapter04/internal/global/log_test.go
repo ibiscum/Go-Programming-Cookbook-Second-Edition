2c1d55bbe6ee88ab30e8002fe36f22fe4c648f4a
ibiscum <ulf.luecking@gmail.com>
2024-02-24 11:25:43 +0100

    Coverage.

--- chapter04/internal/global/log_test.go
+++ chapter04/internal/global/log_test.go
@@ -7,7 +7,7 @@ func TestUseLog(t *testing.T) {
 		name    string
 		wantErr bool
 	}{
-		{"base-case", false},
+		{"base-case", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
